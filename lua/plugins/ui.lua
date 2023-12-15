627b108004e2082331e23f6568442e047c107520
Johann Og <kronolynx@gmail.com>
2023-12-15 13:08:19 +0100

    Updated blank line
    Remove scope underline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -41,72 +41,14 @@ return {
       -- refer to the configuration section below
     },
   },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   {
-    "nvim-tree/nvim-tree.lua", -- increases hyperfine from ~47 to ~80
-    lazy = false,
-    enabled = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup {
-        disable_netrw = false,
-        hijack_netrw = false,
-      }
-    end,
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      scope = {
+        enabled = true,
+        show_start = false
+      },
+    }
   },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<leader>ft", "<cmd>Neotree toggle<CR>", desc = "Neotree toggle" },
-      { "<leader>ff", "<cmd>Neotree reveal<CR>", desc = "Neotree toggle" }
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    deactivate = function()
-      vim.cmd([[Neotree close]])
-    end,
-    config = function()
-      -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-      -- TODO determine if I want this or to configure in in default components, this is general so move it to options
-      --vim.fn.sign_define("DiagnosticSignError",
-      --  { text = " ", texthl = "DiagnosticSignError" })
-      --vim.fn.sign_define("DiagnosticSignWarn",
-      --  { text = " ", texthl = "DiagnosticSignWarn" })
-      --vim.fn.sign_define("DiagnosticSignInfo",
-      --  { text = " ", texthl = "DiagnosticSignInfo" })
-      --vim.fn.sign_define("DiagnosticSignHint",
-      --  { text = "󰌵", texthl = "DiagnosticSignHint" })
-
-      require("neo-tree").setup({
-        filesystem = {
-          hijack_netrw_behavior = "disabled" -- "open_default", "open_current", "disabled",
-        },
-        default_component_configs = {
-          name = {
-            use_git_status_colors = false
-          },
-          diagnostics = {
-            symbols = {
-              hint = "󰌵",
-              info = " ",
-              warn = " ",
-              error = "",
-            },
-            highlights = {
-              hint = "DiagnosticSignHint",
-              info = "DiagnosticSignInfo",
-              warn = "DiagnosticSignWarn",
-              error = "DiagnosticSignError",
-            },
-          },
-        },
-      })
-    end
-  }
 }
