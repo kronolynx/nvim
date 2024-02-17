73cd5b42324907bc1aed03b3fd0065d4bb289704
Johann Og <kronolynx@gmail.com>
2024-02-17 23:02:11 +0100

    Fix cmp to use snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,17 +2,18 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    { "hrsh7th/cmp-emoji" },
     { "hrsh7th/cmp-buffer" },
     { "hrsh7th/cmp-cmdline" },
+    { "hrsh7th/cmp-emoji" },
     { "hrsh7th/cmp-nvim-lsp" },
-    -- { "hrsh7th/cmp-nvim-lsp-signature-help" },
+    { "hrsh7th/cmp-nvim-lsp-signature-help" },
     { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-vsnip" },
-    { "hrsh7th/vim-vsnip" },
-    { "saadparwaiz1/cmp_luasnip" },
-    { "lukas-reineke/cmp-under-comparator" },
     { "onsails/lspkind.nvim" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { 'f3fora/cmp-spell' },
+    { 'hrsh7th/cmp-cmdline' },
+    { 'ray-x/cmp-treesitter' },
+    { "hrsh7th/cmp-nvim-lua" },
     {
       "windwp/nvim-autopairs",
       config = function()
@@ -30,7 +31,7 @@ return {
     local luasnip = require("luasnip")
     local cmp = require("cmp")
     local lspkind = require('lspkind')
-
+    lspkind.init()
 
     cmp.setup({
       snippet = {
@@ -41,6 +42,8 @@ return {
       mapping = {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<Down>'] = cmp.mapping.select_next_item(),
+        ['<Up>'] = cmp.mapping.select_prev_item(),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete {},
@@ -80,21 +83,29 @@ return {
         completeopt = "menu,menuone,noinsert",
       },
       sources = {
-        -- { name = "nvim_lsp_signature_help", group_index = 1 },
-        { name = "nvim_lsp",                priority = 10,  group_index = 1 },
-        { name = 'cmp_vsnip' },           -- Add cmp_vsnip as a source
-        { name = "rg",                      group_index = 2 }, --ripgrep results
-        { name = "nvim_lua",                group_index = 1 },
-        { name = "luasnip",                 group_index = 1 },
-        { name = "buffer",                  group_index = 2 },
-        --{ name = "look",                    keyword_length = 3, option = { convert_case = true, loud = true } },
-        { name = "path",                    group_index = 2 },
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "luasnip",                keyword_length = 3, max_item_count = 3 },
+        { name = "buffer",                 keyword_length = 5, max_item_count = 3 },
+        { name = 'spell' },
+        { name = 'treesitter',             keyword_length = 5, max_item_count = 3 },
+        { name = "nvim_lua",               group_index = 1 },
       },
       formatting = {
         format = lspkind.cmp_format({
           mode = 'symbol',       -- show only symbol annotations
           maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
           ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          menu = {
+            luasnip = "[snip]",
+            nvim_lsp = "[LSP]",
+            buffer = "[buf]",
+            path = "[path]",
+            spell = "[spell]",
+            tags = "[tag]",
+            treesitter = "[TS]",
+            emoji = "[emoji]",
+          },
 
           -- The function below will be called before any actual modifications from lspkind
           -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
@@ -108,6 +119,24 @@ return {
           border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
         },
       },
+    })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      })
     })
   end
 }
