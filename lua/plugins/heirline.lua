ef22088e22c5fea9ff1ba67a447e9b0a8f013e25
Johann Og <kronolynx@gmail.com>
2024-02-19 23:36:39 +0100

    Add toggleterm

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -340,102 +340,6 @@ return {
       -- }
       -- Awesome plugin
 
-      -- Full nerd (with icon colors and clickable elements)!
-      -- works in multi window, but does not support flexible components (yet ...)
-      -- local Navic = {
-      --   condition = function() return require("nvim-navic").is_available() end,
-      --   static = {
-      --     -- create a type highlight map
-      --     type_hl = {
-      --       File = "Directory",
-      --       Module = "@include",
-      --       Namespace = "@namespace",
-      --       Package = "@include",
-      --       Class = "@structure",
-      --       Method = "@method",
-      --       Property = "@property",
-      --       Field = "@field",
-      --       Constructor = "@constructor",
-      --       Enum = "@field",
-      --       Interface = "@type",
-      --       Function = "@function",
-      --       Variable = "@variable",
-      --       Constant = "@constant",
-      --       String = "@string",
-      --       Number = "@number",
-      --       Boolean = "@boolean",
-      --       Array = "@field",
-      --       Object = "@type",
-      --       Key = "@keyword",
-      --       Null = "@comment",
-      --       EnumMember = "@field",
-      --       Struct = "@structure",
-      --       Event = "@keyword",
-      --       Operator = "@operator",
-      --       TypeParameter = "@type",
-      --     },
-      --     -- bit operation dark magic, see below...
-      --     enc = function(line, col, winnr)
-      --       return bit.bor(bit.lshift(line, 16), bit.lshift(col, 6), winnr)
-      --     end,
-      --     -- line: 16 bit (65535); col: 10 bit (1023); winnr: 6 bit (63)
-      --     dec = function(c)
-      --       local line = bit.rshift(c, 16)
-      --       local col = bit.band(bit.rshift(c, 6), 1023)
-      --       local winnr = bit.band(c, 63)
-      --       return line, col, winnr
-      --     end
-      --   },
-      --   init = function(self)
-      --     local data = require("nvim-navic").get_data() or {}
-      --     local children = {}
-      --     -- create a child for each level
-      --     for i, d in ipairs(data) do
-      --       -- encode line and column numbers into a single integer
-      --       local pos = self.enc(d.scope.start.line, d.scope.start.character, self.winnr)
-      --       local child = {
-      --         {
-      --           provider = d.icon,
-      --           hl = self.type_hl[d.type],
-      --         },
-      --         {
-      --           -- escape `%`s (elixir) and buggy default separators
-      --           provider = d.name:gsub("%%", "%%%%"):gsub("%s*->%s*", ''),
-      --           -- highlight icon only or location name as well
-      --           hl = self.type_hl[d.type],
-      --
-      --           on_click = {
-      --             -- pass the encoded position through minwid
-      --             minwid = pos,
-      --             callback = function(_, minwid)
-      --               -- decode
-      --               local line, col, winnr = self.dec(minwid)
-      --               vim.api.nvim_win_set_cursor(vim.fn.win_getid(winnr), { line, col })
-      --             end,
-      --             name = "heirline_navic",
-      --           },
-      --         },
-      --       }
-      --       -- add a separator only if needed
-      --       if #data > 1 and i < #data then
-      --         table.insert(child, {
-      --           provider = " > ",
-      --           hl = { fg = colors.bright_fg },
-      --         })
-      --       end
-      --       table.insert(children, child)
-      --     end
-      --     -- instantiate the new child, overwriting the previous one
-      --     self.child = self:new(children, 1)
-      --   end,
-      --   -- evaluate the children containing navic components
-      --   provider = function(self)
-      --     return self.child:eval()
-      --   end,
-      --   hl = { fg = colors.gray },
-      --   update = 'CursorMoved'
-      -- }
-
       local Diagnostics = {
 
         condition = conditions.has_diagnostics,
@@ -444,7 +348,7 @@ return {
           error_icon = ' ',
           warn_icon = ' ',
           info_icon = ' ',
-          hint_icon = '?',
+          hint_icon = '?', -- TODO replace with bulb
         },
 
         init = function(self)
@@ -612,7 +516,7 @@ return {
           return conditions.buffer_matches({ buftype = { "terminal" } })
         end,
 
-        hl = { bg = colors.dark_red },
+        hl = { bg = colors.bright_bg },
 
         -- Quickly add a condition to the ViMode to only show it when buffer is active!
         { condition = conditions.is_active, ViMode, Space },
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  'akinsho/toggleterm.nvim',
+  opts = {
+    -- direction = 'horizontal',
+    direction = 'float',
+    open_mapping = [[<c-\>]],
+
+  }
+}
