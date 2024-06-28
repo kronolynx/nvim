1e17904538c73655f78b3cb891fa8ead48030a3c
Johann Og <kronolynx@gmail.com>
2024-06-28 21:55:52 +0200

    Update autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,29 +1,80 @@
-local cmd = vim.cmd
+-- Set text width to 80 for markdown files
+vim.api.nvim_create_autocmd('FileType', {
+    group = vim.api.nvim_create_augroup('markdown_settings', { clear = true }),
+    pattern = '*.md',
+    callback = function()
+        vim.bo.textwidth = 80
+    end
+})
+local filetype_settings = vim.api.nvim_create_augroup('filetype_settings', { clear = true })
+-- Set wrap, linebreak, nolist, spell, spelllang, and complete options for markdown, text, and COMMIT_EDITMSG files
+vim.api.nvim_create_autocmd({'BufReadPost','BufNewFile'}, {
+    group = filetype_settings,
+    pattern = '*.md,*.txt,COMMIT_EDITMSG',
+    callback = function()
+        vim.wo.wrap = true
+        vim.wo.linebreak = true
+        vim.wo.list = false
+        vim.bo.spell = true
+        vim.bo.spelllang = 'en_us'
+        vim.bo.complete = vim.bo.complete .. ',kspell'
+    end
+})
 
-cmd([[autocmd FileType markdown setlocal textwidth=80]])
-cmd(
-  [[autocmd BufReadPost,BufNewFile *.md,*.txt,COMMIT_EDITMSG set wrap linebreak nolist spell spelllang=en_us complete+=kspell]]
-)
-cmd([[autocmd BufReadPost,BufNewFile .html,*.txt,*.md,*.adoc set spell spelllang=en_us]])
+-- Set spell and spelllang options for html, text, markdown, and adoc files
+vim.api.nvim_create_autocmd({'BufReadPost','BufNewFile'}, {
+    group = filetype_settings,
+    pattern = '.html,*.txt,*.md,*.adoc',
+    callback = function()
+        vim.bo.spell = true
+        vim.bo.spelllang = 'en_us'
+    end
+})
 
--- Return to last edit position when opening files (You want this!)
-cmd([[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+    group = vim.api.nvim_create_augroup('edit_position', { clear = true }),
+    pattern = '*',
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.cmd("normal! g'\"")
+        end
+    end
+})
 
--- save on focus lost
-cmd([[au FocusLost * if &modified | silent! :wall | endif]])
-cmd([[au BufLeave * if &modified | silent! :wall | endif]])
--- cmd([[au InsertLeave * if &modified | silent! :up | endif ]])
+vim.api.nvim_create_autocmd({'FocusLost', 'BufLeave', 'InsertLeave' }, {
+    group = vim.api.nvim_create_augroup('autosave', { clear = true }),
+    pattern = '*',
+    callback = function()
+        if vim.bo.modified then
+            vim.cmd('silent update')
+        end
+    end
+})
 
-cmd([[au TextYankPost * silent! lua vim.highlight.on_yank {higroup='Visual', timeout=300}]]) -- yank highlight
-
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('yank_highlight', { clear = true }),
+    desc = 'Highlight on yank',
+    callback = function()
+        vim.highlight.on_yank { higroup = 'Visual', timeout = 300 }
+    end,
+})
 
 -- When vimwindow is resized resize splits
 -- cmd([[au VimResized * exe "normal! \<c-w>="]])
 
--- auto-reload files when modified externally
--- https://unix.stackexchange.com/a/383044
-vim.o.autoread = true
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-	command = "if mode() != 'c' | checktime | endif",
-	pattern = { "*" },
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('close_with_q', { clear = true }),
+  desc = 'Close with <q>',
+  pattern = {
+    'help',
+    'man',
+    'qf',
+    'query',
+    'scratch',
+    'spectre_panel',
+  },
+  callback = function(args)
+    vim.keymap.set('n', 'q', '<cmd>quit<cr>', { buffer = args.buf })
+  end,
 })
