f4b6b0cbd9323478ee9d797acc9553e4bdee593d
Johann Og <kronolynx@gmail.com>
2024-06-28 21:59:02 +0200

    Update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,47 +7,59 @@ local global_opt = vim.opt_global
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 g.mapleader = ' '
 g.maplocalleader = ' '
- -- disable netrw
+-- disable netrw
 g.loaded_netrwPlugin = 1
 
 global_opt.scrolloff = 7
 
 opt.clipboard = { "unnamed", "unnamedplus" } -- use the system clipboard
-opt.history = 1000                         -- store the last 1000 commands entered
+opt.history = 1000                           -- store the last 1000 commands entered
 
 -- searching
-opt.ignorecase = true  -- case insensitive searching
-opt.smartcase = true   -- case-sensitive if expresson contains a capital letter
-opt.hlsearch = true    -- highlight search results
-opt.incsearch = true   -- set incremental search, like modern browsers
+opt.ignorecase = true    -- case insensitive searching
+opt.smartcase = true     -- case-sensitive if expresson contains a capital letter
+opt.hlsearch = true      -- highlight search results
+opt.incsearch = true     -- set incremental search, like modern browsers
+opt.inccommand = "split" -- show live preview of substitution
 
-opt.lazyredraw = false -- don't redraw while executing macros
-opt.magic = true       -- set magic on, for regular expressions
+opt.undofile = true      -- save undo history to file
 
-opt.conceallevel = 2   -- show conceals
+opt.lazyredraw = true    -- don't redraw while executing macros
+opt.magic = true         -- set magic on, for regular expressions
+
+opt.showmode = false
+
+opt.conceallevel = 2 -- show conceals
 -- disable wrap of long lines
 opt.wrap = false
 
+-- TODO fix scroll size to 1/3 or 1/4 of the window height
+-- opt.scroll = vim.fn.winheight(0) / 3
+-- opt.scroll = 10 -- math.floor(vim.fn.winheight(0) / 4)
+-- vim.o.scroll = 10 -- math.floor(vim.fn.winheight(0) / 4)
+
 -- indentation
 local indent = 2
-opt.expandtab = true   -- Expand tabs to spaces
+opt.expandtab = true -- Expand tabs to spaces
 opt.tabstop = indent
 opt.shiftwidth = indent
 opt.softtabstop = indent
 opt.expandtab = true
 
+opt.showmode = false --
+
 -- interface
-opt.showcmd = true      -- Show the (partial) command as it’s being typed
-opt.number = true    -- show line numbers
-opt.cmdheight = 0       -- Height of the command bar
-opt.cursorline = true   -- Highlight current line
+opt.showcmd = true    -- Show the (partial) command as it’s being typed
+opt.number = true     -- show line numbers
+opt.cmdheight = 0     -- Height of the command bar -- TODO fix problems with messages and flickering
+opt.cursorline = true -- Highlight current line
 opt.signcolumn = "yes"
 
 -- toggle invisible characters
 opt.list = true
 opt.listchars = {
-  tab = "→ ",
-  -- eol = "¬",
+  tab = " →",
+  eol = "¬",
   trail = "⋅",
   extends = "❯",
   precedes = "❮"
@@ -57,13 +69,32 @@ opt.listchars = {
 -- opt.wildmode = "list:longest,full"
 -- opt.wildmenu = true
 
+-- opt.shortmess:append("c") -- "match 1 of 2", "The only match", "Pattern not found", "Back at original", etc.
+-- opt.shortmess:append("s") -- hit BOTTOM
+-- opt.shortmess:append("A") -- ATTENTION swap
+-- opt.shortmess:append("I") -- intro message when starting
+
 -- suppress messages
--- :h shortmess 
-opt.shortmess:append("c") -- "match 1 of 2", "The only match", "Pattern not found", "Back at original", etc.
-opt.shortmess:append("s") -- hit BOTTOM
-opt.shortmess:append("A") -- ATTENTION swap
-opt.shortmess:append("I") -- intro message when starting
+-- :h shortmess
+opt.shortmess:append {
+  A = true, -- ATTENTION swap
+  F = true, -- don't give the file info when editing a file, like `:silent was used`.
+  I = true, -- intro message when starting
+  W = true, -- don't give "written" or "[w]" when writing a file
+  q = true, -- don't show recording info
+  c = true, -- "match 1 of 2", "The only match", "Pattern not found", "Back at original", etc.
+  n = true, -- no write since last change
+  o = true, -- overwriting a file
+  s = true, -- hit BOTTOM
+}
+
+-- auto-reload files when modified externally
+-- https://unix.stackexchange.com/a/383044
+vim.o.autoread = true
 
 --  delay (in milliseconds) after which the cursor is considered idle and triggers certain events, such as the CursorHold event
 vim.api.nvim_set_option('updatetime', 300)
 
+-- Use ripgrep for grepping.
+vim.o.grepprg = 'rg --vimgrep'
+vim.o.grepformat = '%f:%l:%c:%m'
