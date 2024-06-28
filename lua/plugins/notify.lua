b50d189f734e1673a6c57d2f5cb320b8ab73c462
Johann Og <kronolynx@gmail.com>
2024-06-28 23:33:47 +0200

    Add nvim-notify

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,41 @@
+return {
+  'rcarriga/nvim-notify',
+  enable = true,
+  keys = {
+    {
+      '<leader>nd',
+      function()
+        require('notify').dismiss({ silent = true, pending = true })
+      end,
+      desc = 'Dismiss All Notifications',
+    },
+    {
+      '<leader>nh',
+      function()
+        require("notify").history()
+      end,
+      desc = "Show Notification History"
+    }
+  },
+  init = function()
+    local nvim_notify = require("notify")
+
+    nvim_notify.setup {
+      -- Animation style
+      stages = "fade_in_slide_out",
+      -- Default timeout for notifications
+      timeout = 3000,
+    }
+
+    vim.notify = nvim_notify
+    -- override the default print function to use nvim-notify
+    print = function(...)
+      local print_safe_args = {}
+      local _ = { ... }
+      for i = 1, #_ do
+        table.insert(print_safe_args, tostring(_[i]))
+      end
+      vim.notify(table.concat(print_safe_args, ' '), "info")
+    end
+  end
+}
