1f8725b3dc0fabb830aa87596d8719b230c614bd
Johann Og <kronolynx@gmail.com>
2025-08-21 14:04:29 +0200

    Update snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,185 @@
+local gitbrowse    = {
+  -- what = "permalink",
+  url_patterns = {
+    ["github%.com"] = {
+      branch = "/tree/{branch}",
+      file = "/blob/{branch}/{file}#L{line_start}-L{line_end}",
+      permalink = "/blob/{commit}/{file}#L{line_start}-L{line_end}",
+      commit = "/commit/{commit}",
+    },
+    ["gitlab%.com"] = {
+      branch = "/-/tree/{branch}",
+      file = "/-/blob/{commit}/{file}#L{line_start}-L{line_end}",
+      permalink = "/-/blob/{commit}/{file}#L{line_start}-L{line_end}",
+      commit = "/-/commit/{commit}",
+    },
+    -- ["gitlab.evolution%.com"] = {
+    --   branch = "/-/tree/{branch}",
+    --   file = "/-/blob/{branch}/{file}#L{line_start}-L{line_end}",
+    --   permalink = "/-/blob/{commit}/{file}#L{line_start}-L{line_end}",
+    --   commit = "/-/commit/{commit}",
+    -- },
+    ["bitbucket%.org"] = {
+      branch = "/src/{branch}",
+      file = "/src/{branch}/{file}#lines-{line_start}-L{line_end}",
+      permalink = "/src/{commit}/{file}#lines-{line_start}-L{line_end}",
+      commit = "/commits/{commit}",
+    },
+    ["git.sr.ht"] = {
+      branch = "/tree/{branch}",
+      file = "/tree/{branch}/item/{file}",
+      permalink = "/tree/{commit}/item/{file}#L{line_start}",
+      commit = "/commit/{commit}",
+    },
+  }
+}
+
+local indent       = {
+  enabled = true,
+  scope = {
+    hl = "SnacksDashboardTerminal",
+    priority = 200,
+    char = "│",
+    underline = false,   -- underline the start of the scope
+    only_current = true, -- only show scope in the current window
+  },
+}
+
+local dashboard    = {
+  enabled = false
+  -- sections = {
+  --   { section = "header" },
+  --   { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+  --   { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+  --   { section = "startup" },
+  -- },
+}
+
+local my_layout    = {
+  layout = {
+    box = "vertical",
+    backdrop = false,
+    width = 0.8,
+    min_width = 120,
+    height = 0.9,
+    {
+      win = "preview",
+      title = "{preview}",
+      border = "rounded",
+      height = 0.6,
+    },
+    {
+      box = "vertical",
+      border = "rounded",
+      -- height = 0.,
+      title = "{title} {live} {flags}",
+      { win = "input", height = 1,     border = "bottom" },
+      { win = "list",  border = "none" },
+    },
+  }
+}
+
+local picker       = {
+  matcher = {
+    frecency = true,      -- frecency bonus
+    history_bonus = true, -- give more weight to chronological order
+  },
+  formatters = {
+    file = {
+      filename_first = true,
+      truncate = 200
+    },
+  },
+  ui_select = true, -- replace `vim.ui.select` with the snacks picker
+  sources = {
+    explorer = {
+      auto_close = true,
+      formatters = {
+        file = { filename_only = true },
+        severity = { pos = "right" },
+      },
+      layout = {
+        preset = "sidebar",
+        preview = "main",
+        layout = {
+          width = 60, -- TODO set with to match text
+          -- width = 0.3
+        },
+      },
+    },
+    autocmds = { layout = my_layout },
+    buffers = { layout = my_layout },
+    cliphist = { layout = my_layout },
+    colorschemes = { layout = my_layout },
+    command_history = { layout = my_layout },
+    commands = { layout = my_layout },
+    diagnostics = { layout = my_layout },
+    diagnostics_buffer = { layout = my_layout },
+    files = { layout = my_layout },
+    git_branches = { layout = my_layout },
+    git_files = { layout = my_layout },
+    git_grep = { layout = my_layout },
+    git_log = { layout = my_layout },
+    git_log_file = { layout = my_layout },
+    git_log_line = { layout = my_layout },
+    git_stash = { layout = my_layout },
+    git_status = { layout = my_layout },
+    git_diff = { layout = my_layout },
+    grep = { layout = my_layout },
+    grep_buffers = { layout = my_layout },
+    grep_word = { layout = my_layout },
+    help = { layout = my_layout },
+    highlights = { layout = my_layout },
+    -- icons = { layout = my_layout },
+    jumps = { layout = my_layout },
+    keymaps = { layout = my_layout },
+    lazy = { layout = my_layout },
+    -- lines = { layout = my_layout },
+    loclist = { layout = my_layout },
+    lsp_config = { layout = my_layout },
+    lsp_declarations = { layout = my_layout },
+    lsp_definitions = { layout = my_layout },
+    lsp_implementations = { layout = my_layout },
+    lsp_references = { layout = my_layout },
+    lsp_symbols = { layout = my_layout },
+    lsp_workspace_symbols = { layout = my_layout },
+    lsp_type_definitions = { layout = my_layout },
+    man = { layout = my_layout },
+    marks = { layout = my_layout },
+    -- notifications = { layout = my_layout },
+    -- pickers = { layout = my_layout },
+    -- picker_actions = { layout = my_layout },
+    -- picker_format = { layout = my_layout },
+    -- picker_layouts = { layout = my_layout },
+    -- picker_preview = { layout = my_layout },
+    projects = { layout = my_layout },
+    qflist = { layout = my_layout },
+    recent = { layout = my_layout },
+    registers = { layout = my_layout },
+    resume = { layout = my_layout },
+    search_history = { layout = my_layout },
+    -- select = { layout = my_layout },
+    smart = { layout = my_layout },
+    spelling = { layout = my_layout },
+    treesitter = { layout = my_layout },
+    undo = { layout = my_layout },
+    zoxide = { layout = my_layout },
+  }
+}
+
+local explorer     = {
+  replace_netrw = true,
+}
+
+local statuscolumn = {
+  enabled = true,
+  folds = {
+    open = false,  -- show open fold icons
+    git_hl = true, -- use Git Signs hl for fold icons
+  },
+}
+
+
 return {
   "folke/snacks.nvim",
   enabled = true,
@@ -7,155 +189,29 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     animate = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     bigfile = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
-    dashboard = {
-      enabled = false
-      -- sections = {
-      --   { section = "header" },
-      --   { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-      --   { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-      --   { section = "startup" },
-      -- },
-    },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
+    dashboard = dashboard,
     dim = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
-    gitbrowse = {
-      -- what = "permalink",
-      url_patterns = {
-        ["github%.com"] = {
-          branch = "/tree/{branch}",
-          file = "/blob/{branch}/{file}#L{line_start}-L{line_end}",
-          permalink = "/blob/{commit}/{file}#L{line_start}-L{line_end}",
-          commit = "/commit/{commit}",
-        },
-        ["gitlab%.com"] = {
-          branch = "/-/tree/{branch}",
-          file = "/-/blob/{commit}/{file}#L{line_start}-L{line_end}",
-          permalink = "/-/blob/{commit}/{file}#L{line_start}-L{line_end}",
-          commit = "/-/commit/{commit}",
-        },
-        -- ["gitlab.evolution%.com"] = {
-        --   branch = "/-/tree/{branch}",
-        --   file = "/-/blob/{branch}/{file}#L{line_start}-L{line_end}",
-        --   permalink = "/-/blob/{commit}/{file}#L{line_start}-L{line_end}",
-        --   commit = "/-/commit/{commit}",
-        -- },
-        ["bitbucket%.org"] = {
-          branch = "/src/{branch}",
-          file = "/src/{branch}/{file}#lines-{line_start}-L{line_end}",
-          permalink = "/src/{commit}/{file}#lines-{line_start}-L{line_end}",
-          commit = "/commits/{commit}",
-        },
-        ["git.sr.ht"] = {
-          branch = "/tree/{branch}",
-          file = "/tree/{branch}/item/{file}",
-          permalink = "/tree/{commit}/item/{file}#L{line_start}",
-          commit = "/commit/{commit}",
-        },
-      }
-    },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
-    indent = {
-      enabled = true,
-      scope = {
-        hl = "SnacksDashboardTerminal",
-        priority = 200,
-        char = "│",
-        underline = false,   -- underline the start of the scope
-        only_current = true, -- only show scope in the current window
-      },
-    },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
+    explorer = explorer,
+    gitbrowse = gitbrowse,
+    indent = indent,
     input = {},
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     lazygit = { enabled = true },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
-    notifier = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
-    picker = {
-      matcher = {
-        frecency = true,      -- frecency bonus
-        history_bonus = true, -- give more weight to chronological order
-      },
-      layout = {
-        layout = {
-          box = "vertical",
-          width = 0.8,
-          min_width = 120,
-          height = 0.9,
-          {
-            win = "preview",
-            title = "{preview}",
-            border = "rounded",
-            height = 0.6,
-          },
-          {
-            box = "vertical",
-            border = "rounded",
-            -- height = 0.,
-            title = "{title} {live} {flags}",
-            { win = "input", height = 1,     border = "bottom" },
-            { win = "list",  border = "none" },
-          },
-        },
-      },
-      formatters = {
-        file = {
-          filename_first = true,
-          truncate = 200
-        },
-        -- severity = {
-        --   pos = "right"
-        -- }
-      },
-      ui_select = true,   -- replace `vim.ui.select` with the snacks picker
-      sources = {
-        explorer = {
-          auto_close = true,
-          layout = { preset = "sidebar", preview = false },
-          formatters = {
-            file = { filename_only = true },
-            severity = { pos = "right" },
-          },
-        },
-      }
-    },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
+    notifier = { enabled = true },
+    picker = picker,
     profiler = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     quickfile = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     scope = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     scratch = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     scroll = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
-    statuscolumn = {
-      enabled = true,
-      folds = {
-        open = false,  -- show open fold icons
-        git_hl = true, -- use Git Signs hl for fold icons
-      },
-    },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
+    statuscolumn = statuscolumn,
     styles = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     terminal = { enabled = true },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     toggle = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     win = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     words = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
     zen = { enabled = false },
-    -- ────────────────────────────────────────────────────────────────────────────────────────
   },
   keys = {
     --
@@ -214,7 +270,7 @@ return {
       "<M-e>",
       function()
         Snacks.explorer(
-          { layout = { preset = "sidebar", preview = false } }
+        -- { layout = { preset = "sidebar", preview = false } }
         )
       end,
       desc = "File Explorer"
