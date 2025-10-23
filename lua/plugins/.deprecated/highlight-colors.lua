48d8ed4388535bef37f1f8b503b1ebf943209432
Johann Og <kronolynx@gmail.com>
2025-10-23 22:19:56 +0200

    Deprecate highligh colors

--- lua/plugins/.deprecated/highlight-colors.lua
+++ lua/plugins/.deprecated/highlight-colors.lua
@@ -1,6 +1,7 @@
 return {
   "brenoprata10/nvim-highlight-colors",
   event = "VeryLazy",
+  enabled = false,
   config = function ()
     require('nvim-highlight-colors').setup({
       render = "virtual",
