69a116108511915fcd1073cb5cf2c3e7ad66ae9c
Johann Og <kronolynx@gmail.com>
2025-11-02 12:28:29 +0100

    Add confirm false to markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
   { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" }
-})
+}, { load = true, confirm = false })
 
 vim.defer_fn(function()
   require("render-markdown").setup({
