5ccf6b7f01e8ee6ccab468c8c39c35b41a3d9e64
Johann Og <kronolynx@gmail.com>
2025-11-10 10:51:52 +0100

    Update autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -6,17 +6,32 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
     local line_count = vim.api.nvim_buf_line_count(args.buf)
     if mark[1] > 0 and mark[1] <= line_count then
-      vim.cmd 'normal! g`"zz'
+      vim.schedule(function()
+        vim.cmd 'normal! g`"zz'
+      end)
     end
   end,
 })
 
--- close quickfix menu after selecting choice
-vim.api.nvim_create_autocmd(
-  "FileType", {
-    pattern = { "qf" },
-    command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
-  })
+-- auto resize splits when the terminal's window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+	command = "wincmd =",
+})
+
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
+
+-- -- close quickfix menu after selecting choice
+-- vim.api.nvim_create_autocmd(
+--   "FileType", {
+--     pattern = { "qf" },
+--     command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
+--   })
 
 
 -- auto-read files when modified externally
